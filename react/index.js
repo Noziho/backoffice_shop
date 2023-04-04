a817b9cb700ef60f41486a012abf240477fc5b95
Noziho <noah.decroix3@gmail.com>
2023-04-04 15:18:41 +0200

    Fix reactDOM render

--- react/index.js
+++ react/index.js
@@ -4,13 +4,14 @@ import { Home } from "./pages/home/Home";
 import { UserAccount } from "./pages/UserAccount/UserAccount";
 import { Promotions } from "./pages/promotions/Promotions";
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="user-account" element={<UserAccount />} />
         <Route path="/promotions" element={<Promotions />} />
       </Routes>
-    </BrowserRouter>,
-    document.getElementById("root")
+    </BrowserRouter>
 );
